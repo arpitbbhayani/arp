8a12d41bb9f1706c277a30b1c644dd9535f0cb6e
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2020-02-14 22:33:03 +0530

    reading image

--- services/encoder.py
+++ services/encoder.py
@@ -1,2 +1,22 @@
-def encode(in_path):
+from PIL import Image
+from services.logging import logger
+
+
+def _read_image(path):
+  img = Image.open(path)
+  pixel_map = img.load()
+  logger.info('the image read from path {} has following properties'.format(path))
+  logger.info('size: {} x {}'.format(*img.size))
+  logger.info('format: {}'.format(img.format))
+  img.close()
+  return img, pixel_map
+
+
+def _get_top_256(pixel_map):
+  return []
+
+
+def encode(path):
+  img, pixel_map = _read_image(path)
+  print(_get_top_256(pixel_map))
   return ""
--- services/logging.py
+++ services/logging.py
@@ -3,8 +3,3 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-handler.setFormatter(formatter)
-logger.addHandler(handler)

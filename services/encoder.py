f0fec8c736bb5beb88cfc2171d5c33060632c076
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2020-02-15 08:03:51 +0530

    quantization done.

--- services/encoder.py
+++ services/encoder.py
@@ -17,18 +17,13 @@ def _read_image(path):
 
 
 def _get_quantized_image(img, pixel_map):
-  image_array = np.array([pixel_map[i,j] for i in range(img.size[0]) for j in range(img.size[1])], dtype=np.float64)
+  image_array = np.array([pixel_map[i,j] for i in range(img.size[0]) for j in range(img.size[1])], dtype=np.int8)
   image_array_sample = shuffle(image_array, random_state=0)[:(len(image_array) * 1)//100]
   kmeans = KMeans(n_clusters=256, random_state=0).fit(image_array_sample)
   labels = kmeans.predict(image_array)
   centers = kmeans.cluster_centers_
   new_data = np.array([centers[labels[i * img.size[1] + j]] for i in range(img.size[0]) for j in range(img.size[1])])
-  print(len(new_data))
-  print(new_data[0])
-  np.asarray(a).reshape(2, 2, 4)
-
-  print(nd2[0][0])
-  return nd2
+  return np.asarray(new_data).reshape(img.size[0], img.size[1], len(pixel_map[0,0]))
 
 
 def encode(path):
